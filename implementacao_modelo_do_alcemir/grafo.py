4339a7ff20a524ed1afa522ddcd1254f482f9f09
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-31 22:50:42 -0300

    feita separação de arquivos

---
+++ implementacao_modelo_do_alcemir/grafo.py
@@ -0,0 +1,15 @@
+class Grafo:
+    def __init__(self, id, vertices, arestas):
+        self.id = id
+        self.vertices = vertices
+        self.arestas = arestas
+    
+class OperacoesGrafo:
+    def __init__(self, grafos :list[Grafo]):
+        self.grafos = grafos
+        
+
+        
+
+        
+
--- implementacao_modelo_do_alcemir/grafos.py
+++ implementacao_modelo_do_alcemir/grafos.py
@@ -1,22 +1,20 @@
 import json
-
-class Grafo:
-    def __init__(self, id, vertices, arestas):
-        self.id = id
-        self.vertices = vertices
-        self.arestas = arestas
+import grafo
 
 # TODO: colocar pra ler de um arquivo passado por parametro no terminal
 file = open('c:/Users/franr/Documents/uespi/periodo 7/teoria dos grafos/atividades-teoria-dos-grafos/implementacao_modelo_do_alcemir/graphs.json')
 dados = json.load(file)
 
-grafos = []
+grafos: list[grafo.Grafo] = []
 # aqui vou ter uma lista com todos os grafos em forma de objeto
-for i in  dados['graphs']:
+for i in dados['graphs']:
     grafos.append(
-        Grafo(
+        grafo.Grafo(
             i['id'],
             i['vertices'],
             i['edges'],
         )
     )
+
+# for i in range(len(grafos)):
+#     print(grafos[i].vertices)
