9806a34136e4dc124d6212418e341ab274c38315
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-03 22:39:24 -0300

    iniciado metodo para verificar se grafo é multigrafo

--- implementacao_modelo_do_alcemir/grafo.py
+++ implementacao_modelo_do_alcemir/grafo.py
@@ -1,15 +1,17 @@
 class Grafo:
     def __init__(self, id, vertices, arestas):
         self.id = id
-        self.vertices = vertices
-        self.arestas = arestas
-    
-class OperacoesGrafo:
-    def __init__(self, grafos :list[Grafo]):
-        self.grafos = grafos
-        
+        self.vertices: list = vertices
+        self.arestas: list = arestas
 
+# class OperacoesGrafo:
+#     def __init__(self, grafos :list[Grafo]):
+#         self.grafos = grafos
         
+def saoMultigrafos(grafos:list[Grafo]):
+    saoMultigrafos: list[bool] = [0 for _ in range(len(grafos))]
 
+    for i in range(len(grafos)):
+        arestasRepetidas = []
         
 
