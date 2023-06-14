ff5a4ec42c52715cc2aefa4a475180576c1adbdb
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-13 22:10:31 -0300

    alterado forma de escrita de função

--- implementacao_definitiva/funcoes/grau.py
+++ implementacao_definitiva/funcoes/grau.py
@@ -1,8 +1,12 @@
 from classes.grafo import Grafo
 
+# def grauDeTodosOsVertices(grafo: Grafo):
+
+
 def grauDeVerticeEspecifico(grafo: Grafo, vertice):
     grau = 0
     for i in range(len(grafo.arestas)):
-        if grafo.arestas[i][0] == vertice or grafo.arestas[i][1] == vertice:
+        # if grafo.arestas[i][0] == vertice or grafo.arestas[i][1] == vertice:
+        if vertice in grafo.arestas[i]: #msm coisa acima
             grau = grau + 1
     print("o grau do vértice ",vertice," é ",grau)
