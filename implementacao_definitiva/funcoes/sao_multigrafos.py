9d0970ec4d7091c25aa8d7446cd5bc14b5e4c000
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-13 21:54:15 -0300

    alterado diretório dos arquivos

---
+++ implementacao_definitiva/funcoes/sao_multigrafos.py
@@ -0,0 +1,17 @@
+from classes.grafo import Grafo
+
+def saoMultigrafos(grafos: list[Grafo]):
+    saoMultigrafos: list[Grafo] = []
+
+    for grafo in grafos:
+        
+        for i in range(len(grafo.matrizADJ)):
+            for j in range(len(grafo.matrizADJ[i])):
+
+                if (grafo.matrizADJ[i][j]>1):
+                    saoMultigrafos.append(grafo)
+                    break
+        
+    print("dentre esses grafos, são multigrafos os com os seguintes IDs:") 
+    for grafo in saoMultigrafos:
+        print(grafo.id)
---
+++ implementacao_definitiva/funcoes/sao_pseudografos.py
@@ -0,0 +1,17 @@
+from classes.grafo import Grafo
+
+def saoPseudografos(grafos: list[Grafo]):
+    saoPseudografos: list[Grafo] = []
+
+    for grafo in grafos:
+
+        for i in range(len(grafo.matrizADJ)):
+            if (grafo.matrizADJ[i][i]>0):
+                    saoPseudografos.append(grafo)
+                    break
+        
+    print("dentre esses grafos, são pseudgrafos os com os seguintes IDs:") 
+    for grafo in saoPseudografos:
+        print(grafo.id)
+
+
