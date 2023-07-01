88ad6b5ccc7e39e7dd9e5e2bfdd887d08805373c
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-30 22:08:46 -0300

    alterada função de encontrar desconexos

--- funcoes/sao_desconexos.py
+++ funcoes/sao_desconexos.py
@@ -1,38 +1,28 @@
 from classes.grafo import Grafo
 
-def verificaConexao(vertices, arestas):
-    if not vertices:
-            return False
-    
-    verticesAlcancaveis = set()
-    queue = [vertices[0]] 
-
-    while queue:
-        verticeAtual = queue.pop(0)
-        verticesAlcancaveis.add(verticeAtual)
-
-        for aresta in arestas:
-            if aresta[0] == verticeAtual and aresta[1] not in verticesAlcancaveis:
-                verticesAlcancaveis.add(aresta[1])
-                queue.append(aresta[1])
-
-            elif aresta[1] == verticeAtual and aresta[0] not in verticesAlcancaveis:
-                verticesAlcancaveis.add(aresta[0])
-                queue.append(aresta[0])
-
-    return len(verticesAlcancaveis) == len(vertices)
+def dfs(grafo, vertice, visitados):
+    visitados.add(vertice)
+    for aresta in grafo.arestas:
+        if vertice in aresta:
+            vizinho = aresta[0] if aresta[0] != vertice else aresta[1]
+            if vizinho not in visitados:
+                dfs(grafo, vizinho, visitados)
 
 def saoDesconexos(grafos: list[Grafo]):
     saoDesconexos = []
 
     for grafo in grafos:
-        vertices = grafo.vertices
-        arestas = grafo.arestas
+        todos_vertices = set(grafo.vertices)
+        visitados = set()
 
-        if not verificaConexao(vertices, arestas):
+        # Encontrar uma raiz (vértice inicial) para iniciar a busca
+        raiz = grafo.vertices[0]
+
+        dfs(grafo, raiz, visitados)
+
+        if todos_vertices != visitados:
             saoDesconexos.append(grafo)
     
     return saoDesconexos
-            
 
 
