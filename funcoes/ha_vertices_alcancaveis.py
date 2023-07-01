15f1e05f7e7a92d81f0f0f410d8e15476cb695cf
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-30 22:23:01 -0300

    alterada função de encontrar vertices alcancaveis

--- funcoes/ha_vertices_alcancaveis.py
+++ funcoes/ha_vertices_alcancaveis.py
@@ -1,13 +1,13 @@
 from classes.grafo import Grafo
 
-def dfs(grafo, vertice, visitados, vertices_alcancaveis):
+def dfs(grafo, vertice, visitados, verticesAlcancaveis):
     visitados.add(vertice)
-    vertices_alcancaveis.append(vertice)
+    verticesAlcancaveis.append(vertice)
     for aresta in grafo.arestas:
         if vertice in aresta:
             vizinho = aresta[0] if aresta[0] != vertice else aresta[1]
             if vizinho not in visitados:
-                dfs(grafo, vizinho, visitados, vertices_alcancaveis)
+                dfs(grafo, vizinho, visitados, verticesAlcancaveis)
 
 def haVerticesAlcancaveis(grafos: list[Grafo], verticeInicial):
     haVerticesAlcancaveis = []
