c5f2498c0c2c24e5ba9644097059bc0a380d2bb1
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-06-30 21:59:27 -0300

    adicionado comentario na funcao de saoCompleto explicando formula

--- funcoes/sao_completos.py
+++ funcoes/sao_completos.py
@@ -8,6 +8,8 @@ def saoCompletos(grafos: list[Grafo]):
         arestas = grafo.arestas
 
         # Verificar se o número de arestas é igual ao número máximo possível de arestas
+        # formula pra saber se um grafo é completo, tendo 'n' vértices:
+        # n * (n - 1) / 2
         if len(arestas) == len(vertices) * (len(vertices) - 1) // 2:
             saoCompletos.append(grafo)  # O grafo é completo
     
